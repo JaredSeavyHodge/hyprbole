04ea2a91398086962f2a6da17678b19647005c9c
Jared Hodge <jaredseavy@gmail.com>
2026-05-18 20:44:39 -0400

    Remove obsolete migration scripts for Ghostty theme path and SwayNC service

--- migrations/20260517-fix-ghostty-theme-path.sh
+++
@@ -1,7 +0,0 @@
-echo "Update Ghostty to load Hyprbole current theme config from ~/.config"
-
-ghostty_config="$HOME/.config/ghostty/config"
-
-if [[ -f $ghostty_config ]]; then
-  sed -i 's|~/.local/share/hyprbole/current/theme/ghostty.conf|~/.config/hyprbole/current/theme/ghostty.conf|g' "$ghostty_config"
-fi
